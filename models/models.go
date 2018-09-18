0e3f85fc4818d917a89522dcfad7470018cd665b
Daniel Wangai <mainad57@gmail.com>
2018-09-18 15:41:54 +0300

    add function to create a bucketlist.

--- models/models.go
+++ models/models.go
@@ -1,6 +1,12 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+)
 
 type BaseModel struct {
 	ID        string `gorm:"primary_key;unique"`
@@ -21,4 +27,16 @@ type Item struct {
 	Description  string `gorm:"not null;size:200"`
 	Bucketlist   Bucketlist
 	BucketlistId string
+}
+
+func CreateBucketlist(name, description string, db *gorm.DB) (*Bucketlist, error) {
+	var bucketlist Bucketlist
+	bucketlist.ID = uuid.Must(uuid.NewV4()).String()
+	bucketlist.Name = name
+	bucketlist.Description = description
+	db.Create(&bucketlist)
+	if len(bucketlist.ID) > 0 {
+		return &bucketlist, nil
+	}
+	return nil, errors.New("An error occured. Create operation unsuccessful.")
 }

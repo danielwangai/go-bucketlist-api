c7ce15a374864b728906deebfa1565fc7a1e3bc7
Daniel Wangai <mainad57@gmail.com>
2018-09-18 15:05:36 +0300

    - add dotenv - handle db connection - add .gitignore

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"go_bucketlist_api/controllers"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	dotenv_err := godotenv.Load() // loads env variables from .env file
+	if dotenv_err != nil {
+		log.Fatal(dotenv_err)
+	}
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_NAME := os.Getenv("DB_NAME")
+	SSL_MODE := os.Getenv("SSL_MODE")
+	db, db_err := gorm.Open("postgres", "host="+DB_HOST+" port=5432 dbname="+DB_NAME+" sslmode="+SSL_MODE)
+	if db_err != nil {
+		fmt.Println("DB Connection ERROR")
+		log.Fatal(db_err)
+	}
+	_ = db
+
 	router := mux.NewRouter()
 	// bucketlist routes
 	router.HandleFunc("/bucketlists", controllers.CreateBucketlist).Methods("POST")

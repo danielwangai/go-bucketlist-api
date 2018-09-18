0023c46ae2ee92a7d651e6c439d8f768918be25f
Daniel Wangai <mainad57@gmail.com>
2018-09-18 14:41:45 +0300

    - create endpoints for bucketlists and items

---
+++ controllers/bucketlists_controller.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func CreateBucketlist(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
+
+func GetAllBucketlists(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
+
+func GetOneBucketlist(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
+
+func UpdateBucketlist(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
+
+func DeleteBucketlist(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
---
+++ controllers/items_controller.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func CreateItem(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
+
+func GetBucketlistItems(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
+
+func GetOneItem(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
+
+func UpdateItem(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}
+
+func DeleteItem(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Not implemented yet.")
+}

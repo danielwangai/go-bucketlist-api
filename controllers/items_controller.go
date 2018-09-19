af96a006e9b713c40f72dc4dd39e888db2acd18a
Daniel Wangai <mainad57@gmail.com>
2018-09-19 17:07:42 +0300

    implement endpoint to create item.

--- controllers/items_controller.go
+++ controllers/items_controller.go
@@ -1,12 +1,36 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
+	"go_bucketlist_api/models"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func CreateItem(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Not implemented yet.")
+	var item models.Item
+	params := mux.Vars(r)
+	bucketlist, bucketErr := models.FetchOneBucketlist(params["id"])
+	if bucketErr != nil {
+		RespondWithError(w, http.StatusNotFound, "Bucketlist not found.")
+		return
+	}
+	if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
+		RespondWithError(w, http.StatusNotFound, "Invalid request payload.")
+		return
+	}
+	if len(item.Description) == 0 {
+		RespondWithError(w, http.StatusNotFound, "Item description cannot be empty.")
+		return
+	}
+	i, itemErr := models.CreateItem(*bucketlist, item.Description)
+	if itemErr != nil {
+		RespondWithError(w, http.StatusNotFound, itemErr.Error())
+		return
+	}
+	RespondWithJson(w, http.StatusCreated, i)
 }
 
 func GetBucketlistItems(w http.ResponseWriter, r *http.Request) {
